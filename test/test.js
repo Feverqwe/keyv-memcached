ba93892e1e36447d64dca9299d566e3d1b34703a
Anton <leonardspbox@gmail.com>
2019-04-04 09:45:52 +0500

    dep upd

--- test/test.js
+++ test/test.js
@@ -6,9 +6,15 @@ import KeyvRedis from 'this';
 const KeyvMemcached = require('../src/keyv-memcached');
 
 keyvOfficialTests(test, Keyv, {
-  store: new KeyvMemcached('memcached://127.0.0.1:11211')
+  store: new KeyvMemcached('memcached://127.0.0.1:11211', {
+    "timeout": 1000,
+    "idle": 1000
+  })
 }, {
-  store: new KeyvMemcached('memcached://127.0.0.1:22122')
+  store: new KeyvMemcached('memcached://127.0.0.1:22122', {
+    "timeout": 1000,
+    "idle": 1000
+  })
 });
 
 const store = () => new KeyvRedis();
